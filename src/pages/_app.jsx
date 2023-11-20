bfa5fb278804fc1e5c5889c5eaffad2664d721c1
Sanyogita <sanyogitavkaps@gmail.com>
2023-11-20 15:12:07 +0530

    add meta

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,23 @@
-import Layout from "@/components/layout";
-import "@/styles/globals.css";
+import Layout from '@/components/layout'
+import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
-}
+    <>
+      <Head>
+        <meta charSet='UTF-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name='description' content='Your page description goes here' />
+        <meta name='keywords' content='Blogs,CMS Blog, Blogs' />
+        <meta name='author' content='Sanyogita'  />
+        <title>Blogs by CMS using contentful</title>
 
+        {/* Add any additional meta tags or links to external resources here */}
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
+}
