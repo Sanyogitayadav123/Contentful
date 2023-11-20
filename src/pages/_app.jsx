943764929e721db05d16b70ab6a6c034fdd63351
Sanyogita <sanyogitavkaps@gmail.com>
2023-11-20 15:28:35 +0530

    add meta tag

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,16 +5,6 @@ import Head from 'next/head'
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <meta charSet='UTF-8' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <meta name='description' content='Your page description goes here' />
-        <meta name='keywords' content='Blogs,CMS Blog, Blogs' />
-        <meta name='author' content='Sanyogita'  />
-        <title>Blogs by CMS using contentful</title>
-
-        {/* Add any additional meta tags or links to external resources here */}
-      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,41 +1,28 @@
-import { client } from "@/lib/contentful/client";
-import { useEffect, useState } from "react";
-import Posts from "./Post";
-// import Posts from "./posts";
+import Posts from './Post'
+import Home from './Home'
+import Head from 'next/head'
 
 
 const Homepage = () => {
-  const[landingPage,setLandingPage] = useState()
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await client.getEntries({ content_type: 'landingPage' })
-        setLandingPage(response?.items)
-      } catch (error) {
-        console.error('Error fetching data:', error)
-      }
-    }
-    fetchData()
-  }, [])
   return (
-    <div>
-      {/* <h1 className="text-3xl font-bold">Home Page</h1> */}
-      {landingPage?.map((value,i)=>{
-        console.log('value', value?.fields?.bannerImg?.fields?.file?.url)
-        return(
-          <div key={i}>
-            <img 
-            className="w-[1254px]"
-            alt="img" src={value?.fields?.bannerImg?.fields?.file?.url}/>
-            </div>
-        )
-      })}
-      <Posts/>
-    </div>
-  );
-};
+    <>
+      <Head>
+        <meta charSet='UTF-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta name='description' content='Your page description goes here' />
+        <meta name='keywords' content='Blogs,CMS Blog, Blogs' />
+        <meta name='author' content='Sanyogita' />
+        <title>Blogs by CMS using contentful</title>
 
+        {/* Add any additional meta tags or links to external resources here */}
+      </Head>
+      <Home/>
+  
+        <Posts />
+ 
+    </>
+  )
+}
 
-
-export default Homepage;
+export default Homepage
