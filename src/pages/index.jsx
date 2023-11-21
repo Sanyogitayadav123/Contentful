65aed3a38971eccc353ae4ac151b778784519c16
Sanyogita <sanyogitavkaps@gmail.com>
2023-11-21 13:31:32 +0530

    add google analytics more add

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,9 +2,7 @@ import Posts from './Post'
 import Home from './Home'
 import Head from 'next/head'
 
-
 const Homepage = () => {
-
   return (
     <>
       <Head>
@@ -13,32 +11,26 @@ const Homepage = () => {
         <meta name='description' content='Your page description goes here' />
         <meta name='keywords' content='Blogs,CMS Blog, Blogs' />
         <meta name='author' content='Sanyogita' />
-        <meta property='og:title' content='My CMS blogs'/>
+        <meta property='og:title' content='My CMS blogs' />
         <title>Blogs by CMS using contentful</title>
-        <link rel='icon' href='/Blog-icon.png'/>
+        <link rel='icon' href='/Blog-icon.png' />
 
         {/* Add any additional meta tags or links to external resources here */}
         <script
           async
-          src={`https://www.googletagmanager.com/gtag/js?id=G-JGJ19FE1C6`}
+          src='https://www.googletagmanager.com/gtag/js?id=G-JGJ19FE1C6'
         ></script>
         <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag() {
-              window.dataLayer.push(arguments);
-            }
-            gtag('js', new Date());
-            gtag('config', 'G-JGJ19FE1C6');
-          `}
-          {/* G-JGJ19FE1C6 */}
-          {/* 6395715437 */}
+          {` window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+   
+        gtag('config', 'G-JGJ19FE1C6');`}
         </script>
       </Head>
-      <Home/>
-  
-        <Posts />
- 
+      <Home />
+
+      <Posts />
     </>
   )
 }
